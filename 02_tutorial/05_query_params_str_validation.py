2cb55d0d0c367cca6a5e3a75f333c948ba05be87
Ridwan Halim <hi@ridwaanhall.com>
2026-01-17 11:15:39 +0700

    feat: add example endpoint with optional query parameter for item retrieval

---
+++ 02_tutorial/05_query_params_str_validation.py
@@ -0,0 +1,14 @@
+# Query Parameters and String Validations
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# example endpoint with optional query parameter
+@app.get("/items/")
+async def read_items(q: str | None = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
