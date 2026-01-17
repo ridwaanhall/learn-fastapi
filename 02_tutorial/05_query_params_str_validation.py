ecaa11d0d646ac4f0c9f8ffc5e68eb1e8d8656f5
Ridwan Halim <hi@ridwaanhall.com>
2026-01-17 11:23:40 +0700

    feat: add string validation for optional query parameter in read_users endpoint

--- 02_tutorial/05_query_params_str_validation.py
+++ 02_tutorial/05_query_params_str_validation.py
@@ -1,6 +1,7 @@
 # Query Parameters and String Validations
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
@@ -9,6 +10,22 @@ app = FastAPI()
 @app.get("/items/")
 async def read_items(q: str | None = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+# example endpoint with string validations on query parameter
+@app.get("/users/")
+async def read_users(
+    q: Annotated[ # Annotated means we can add extra information to the type
+        str | None, # optional query parameter of type str or None, default is None
+        Query(
+            max_length=50 # maximum length of the string is 50 characters
+        ),
+    ] = None
+):
+    results = {"users": [{"username": "johndoe"}, {"username": "alice"}]}
     if q:
         results.update({"q": q})
     return results
