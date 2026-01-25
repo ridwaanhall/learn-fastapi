1058e7bb64581559778a921e466d4980f2835819
Ridwan Halim <hi@ridwaanhall.com>
2026-01-25 13:29:41 +0700

    ✨feat(api): Implement item update endpoint with embedded body fields

---
+++ 02_tutorial/09_body_fields.py
@@ -0,0 +1,36 @@
+# body fields
+
+from typing import Annotated
+
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = Field(
+        default=None,
+        title="The description of the item",
+        max_length=300,
+    )
+    price: float = Field(
+        gt=0,
+        description="The price must be greater than zero",
+    )
+    tax: float | None = None
+    
+    
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            embed=True, # embed the fields of the Item model directly in the request body
+        ),
+    ]
+):
+    results = {"item_id": item_id, "item": item}
+    return results
