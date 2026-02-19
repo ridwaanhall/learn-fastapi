318fdb781b37e67394619f1293bba696848a1480
Ridwan Halim <hi@ridwaanhall.com>
2026-02-19 16:22:02 +0700

    ✨feat(form-data): Add form data handling with login endpoint in FastAPI

---
+++ 02_tutorial/19_form_data.py
@@ -0,0 +1,21 @@
+# form data
+from typing import Annotated
+
+from fastapi import FastAPI, Form
+
+"""
+Use Form to declare form data input parameters.
+tip
+to declare form bodies, you can use Form, which is imported from fastapi.
+coz without it the parameters would be interpreted as query parameters or body (JSON) parameters,
+and not form data.
+"""
+
+app = FastAPI()
+
+@app.post("/login/")
+async def login(
+    username: Annotated[str, Form()],
+    password: Annotated[str, Form()]
+):
+    return {"username": username, "password": password}
